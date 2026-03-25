5dc9a3b4c7eb15aae56435551639c21044f5210d
zhangchunsheng <zhangchunsheng423@gmail.com>
2026-03-25 17:43:34 +0800

    update

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- 底部版权信息 -->
+        <footer class="text-center text-sm text-gray-500 py-4">
+          © 2026 班级宠物园 | <a href="https://www.luomor.com/" target="_blank" class="text-blue-500 hover:underline">烙馍网</a>
+        </footer>
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-6096731848877113"
+            data-ad-slot="7379413361"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+    </div>
+</template>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
